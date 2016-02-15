857c691cdf28ef409d5e3b06360bababd9cd9737
SatoshiShimada <mylinux1204@gmail.com>
2016-02-16 00:58:15 +0900

    Add dropout and momentum
    todo: weight-decay and weight-limit

--- layers/fullyconnected_layer.py
+++ layers/fullyconnected_layer.py
@@ -3,9 +3,9 @@
 # Learning layer
 # Fully connected layer
 #
-# TODO: add dropout, momentum and weight-decay
+# TODO: add weights-decay, weights-limit
 #
-# Date: Feb 13, 2016
+# Date: Feb 15, 2016
 # Author: Satoshi SHIMADA
 
 import numpy
@@ -20,7 +20,7 @@ class FullyConnectedLayer \
         if weights == None:
             weights = numpy.random.randn(output_size, input_size)
         if biases == None:
-            biases = numpy.random.randn(output_size, 1)
+            biases = numpy.zeros((output_size, 1))
         self.weights = weights
         self.biases  = biases
         self.input_size    = input_size
@@ -28,20 +28,41 @@ class FullyConnectedLayer \
         self.activation    = activation
         self.learning_rate = learning_rate
         self.dropout_rate  = dropout_rate
+        self.erase_units   = int(input_size * dropout_rate)
+        self.prev_delta_weights = 0.
 
-    def feedForward(self, inputs):
-        # dropout process
-        self.before_activation = numpy.dot(self.weights, inputs) + self.biases
-        return self.activation.apply(self.before_activation)
+    def feedForward(self, inputs, train=False):
+        self.used_index = []
+        if train:
+            # dropout process
+            for i in xrange(self.erase_units):
+                # delete connection
+                index = numpy.random.randint(self.output_size)
+                while True:
+                    if index in self.used_index:
+                        index = numpy.random.randint(self.output_size)
+                    else:
+                        break
+                inputs[index] = 0.
+            weights = self.weights
+            biases = self.biases
+        else:
+            weights = self.weights * self.dropout_rate 
+            biases = self.biases * self.dropout_rate
+        self.before_activation = numpy.dot(weights, inputs) + biases
+        return self.activation._apply(self.before_activation)
 
     def backPropagation(self, inputs, delta, prev_out=None):
         # calc delta
         if prev_out != None:
-            next_delta = numpy.dot(self.weights.transpose(), delta) * self.activation.diff(prev_out)
+            next_delta = numpy.dot(self.weights.transpose(), delta) * self.activation._diff(prev_out)
         else:
             next_delta = None
-        self.weights -= self.learning_rate * numpy.dot(delta, inputs.transpose())
-        self.biases  -= self.learning_rate * delta
+        delta_weights = self.learning_rate * numpy.dot(delta, inputs.transpose())
+        delta_biases  = self.learning_rate * delta
+        self.weights -= delta_weights + self.prev_delta_weights
+        self.biases  -= delta_biases
+        self.prev_delta_weights = delta_weights
         return next_delta
 
     def getOutput(self):
