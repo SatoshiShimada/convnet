d2426545bc7f3ab301730fba4a3b7268e2060935
SatoshiShimada <mylinux1204@gmail.com>
2016-02-21 10:19:14 +0900

    Fix convolution algorithm
    Modify feed-forward and back-propagation

--- layers/convolution_layer.py
+++ layers/convolution_layer.py
@@ -29,7 +29,7 @@ class ConvolutionLayer \
 
     def feedForward(self, inputs, train=True):
         inputs = inputs.reshape((self.input_dim[2], self.input_dim[1], self.input_dim[0]))
-        out = numpy.zeros((self.output_dim[2], self.output_dim[1], self.output_dim[1]))
+        out = numpy.zeros((self.output_dim[2], self.output_dim[1], self.output_dim[0]))
         for k in xrange(self.filter_dim[2]):
             for i in xrange(self.output_dim[1]):
                 for j in xrange(self.output_dim[0]):
@@ -44,36 +44,44 @@ class ConvolutionLayer \
 
     def backPropagation(self, inputs, delta, prev_out=None):
         inputs = inputs.reshape((self.input_dim[2], self.input_dim[1], self.input_dim[0]))
-        # calculation delta for next layer
         delta = delta.reshape((self.output_dim[2], self.output_dim[1], self.output_dim[0]))
-        #next_delta = numpy.zeros((self.input_dim[2], self.output_dim[1], self.output_dim[0]))
+        before_activation = self.before_activation.reshape((self.output_dim[2], self.output_dim[1], self.output_dim[0]))
+
+        # calculation delta for next layer
         next_delta = numpy.zeros((self.input_dim[2], self.input_dim[1], self.input_dim[0]))
         for c in xrange(self.input_dim[2]):
-            for i in xrange(self.output_dim[1]):
-            #for i in xrange(self.input_dim[1]):
-                for j in xrange(self.output_dim[0]):
-                #for j in xrange(self.input_dim[0]):
+            for i in xrange(self.input_dim[1]):
+                for j in xrange(self.input_dim[0]):
                     for k in xrange(self.filter_dim[2]):
                         for s in xrange(self.filter_dim[1]):
                             for t in xrange(self.filter_dim[0]):
-                                if i - s < 0 or j - t < 0:
+                                #index1 = i - (self.filter_dim[1] - 1) - s
+                                #index2 = j - (self.filter_dim[0] - 1) - t
+                                index1 = i - (self.filter_dim[1]) - s
+                                index2 = j - (self.filter_dim[0]) - t
+                                if index1 < 0 or index2 < 0:
                                     continue
-                                next_delta[c][i][j] += delta[k][i-s][j-t] * self.weights[k][c][s][t]
-        # update parameters
+                                next_delta[c][i][j] += delta[k][index1][index2] * self.activation._diff(before_activation[k][index1][index2] + self.biases[k]) * self.weights[k][c][s][t]
+
         delta_weights = numpy.zeros(self.weights.shape)
         delta_biases  = numpy.zeros(self.biases.shape)
+        # calculation gradient
         for k in xrange(self.filter_dim[2]):
+            for i in xrange(self.output_dim[1]):
+                for j in xrange(self.output_dim[0]):
+                    d = delta[k][i][j] * self.activation._diff(before_activation[k][i][j] + self.biases[k])
+                    delta_biases[k] += d
+                    for c in xrange(self.input_dim[2]):
+                        for s in xrange(self.filter_dim[1]):
+                            for t in xrange(self.filter_dim[0]):
+                                delta_weights[k][c][s][t] += d * inputs[c][i+s][j+t]
+        # update parameters
+        for k in xrange(self.filter_dim[2]):
+            self.biases -= self.learning_rate * delta_biases[k]
             for c in xrange(self.input_dim[2]):
                 for s in xrange(self.filter_dim[1]):
                     for t in xrange(self.filter_dim[0]):
-                        for i in xrange(self.output_dim[1]):
-                            for j in xrange(self.output_dim[0]):
-                                delta_weights[k][c][s][t] += delta[k][i][j] * inputs[c][i+s][j+t]
-            for i in xrange(self.output_dim[1]):
-                for j in xrange(self.output_dim[0]):
-                    delta_biases[k] += delta[k][i][j]
-        self.weights -= delta_weights
-        self.biases  -= delta_biases
+                        self.weights[k][c][s][t] -= self.learning_rate * delta_weights[k][c][s][t]
         return next_delta.reshape((-1, 1))
 
     def getOutput(self):
